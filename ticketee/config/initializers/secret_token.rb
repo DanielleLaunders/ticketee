9f78ee098acd337943dd65b49d004bb2a3d01541
Danielle <Danielle@moz.com>
2013-07-02 18:33:39 -0700

    set up gem dependencies and run rspec generator

---
+++ ticketee/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticketee::Application.config.secret_key_base = '47e5ee38db1d31fa885abefc537a0b86d8eab99427b89cc1980d1571a500f296008edf21c0e0b022a84f28543b90fcbf1c2c8a02bc4faf15ef9827f2982ce392'
